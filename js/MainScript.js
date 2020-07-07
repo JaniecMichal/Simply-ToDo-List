2947fd6f1083760d77b3b9425cff6814e1a0a753
Michal Janiec <michal.janiec95@gmail.com>
2020-07-07 22:53:30 +0200

    Adding task function added, new icons for buttons in TaskListPanel

--- js/MainScript.js
+++ js/MainScript.js
@@ -1,13 +1,49 @@
 {
+    const tasks = [
+        {
+            content: "Obejrzeć film Misja",
+            done: false,
+        },
+        {
+            content: "Pobawić się z Domisiem",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let HTMLstring = "";
+
+        for (const task of tasks) {
+            HTMLstring += `
+            <li class="TaskListToDoPanel__ListItem"> <button class="TaskListToDoPanel__Button ${task.done ? "TaskListToDoPanel__Button--done" : ""}"></button>
+            <p class="TaskListToDoPanel__Task ${task.done ? "TaskListToDoPanel__Task--done" : ""}">${task.content}</p><button
+                class="TaskListToDoPanel__Button TaskListToDoPanel__Button--remove"></button>
+             </li>
+            `;
+        }
+        document.querySelector(".TaskListToDoPanel__TaskList").innerHTML = HTMLstring;
+    };
+
     const OnFormSubmit = (event) => {
         event.preventDefault();
+        const TaskContainer = document.querySelector(".TaskAddingPanel_Input").value.trim();
+        console.log(TaskContainer);
 
+        if (TaskContainer === "") {
+            return;
+        }
+        tasks.push({
+            content: TaskContainer,
+            done: false,
+        })
+        render();
     };
+
     const init = () => {
-        
+
         const form = document.querySelector(".js-form");
-        const addNewTaskButton = document.querySelector(".js-addNewTask");
         form.addEventListener("submit", OnFormSubmit);
+        render();
 
     };
     init();
