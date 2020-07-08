c876095de29fa14cffe3b3f3510d348899dbfe92
Michal Janiec <michal.janiec95@gmail.com>
2020-07-08 20:46:27 +0200

    Added Removing task from list and adding new task refactorizing

--- js/MainScript.js
+++ js/MainScript.js
@@ -10,33 +10,60 @@
         },
     ];
 
+    const addNewTask = (NewTaskContainer) => {
+        tasks.push({
+            content: NewTaskContainer,
+            done: false,
+        })
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const render = () => {
         let HTMLstring = "";
 
         for (const task of tasks) {
             HTMLstring += `
-            <li class="TaskListToDoPanel__ListItem"> <button class="TaskListToDoPanel__Button ${task.done ? "TaskListToDoPanel__Button--done" : ""}"></button>
+            <li class="TaskListToDoPanel__ListItem"> <button class="TaskListToDoPanel__Button js-doneButton ${task.done ? "TaskListToDoPanel__Button--done" : ""}"></button>
             <p class="TaskListToDoPanel__Task ${task.done ? "TaskListToDoPanel__Task--done" : ""}">${task.content}</p><button
                 class="TaskListToDoPanel__Button TaskListToDoPanel__Button--remove"></button>
              </li>
             `;
         }
         document.querySelector(".TaskListToDoPanel__TaskList").innerHTML = HTMLstring;
+
+        const RemoveButtons = document.querySelectorAll(".TaskListToDoPanel__Button--remove");
+
+
+        RemoveButtons.forEach((RemoveButton, index) => {
+            RemoveButton.addEventListener("click", () => {
+                removeTask(index);
+            })
+        });
+
+        const RemoveButtons = document.querySelectorAll(".TaskListToDoPanel__Button--remove");
+
+
+        RemoveButtons.forEach((RemoveButton, index) => {
+            RemoveButton.addEventListener("click", () => {
+                removeTask(index);
+            })
+        });
+
     };
 
     const OnFormSubmit = (event) => {
         event.preventDefault();
-        const TaskContainer = document.querySelector(".TaskAddingPanel_Input").value.trim();
-        console.log(TaskContainer);
+        const NewTaskContainer = document.querySelector(".TaskAddingPanel_Input").value.trim();
 
-        if (TaskContainer === "") {
+        if (NewTaskContainer === "") {
             return;
-        }
-        tasks.push({
-            content: TaskContainer,
-            done: false,
-        })
-        render();
+        };
+        addNewTask(NewTaskContainer);
     };
 
     const init = () => {
